65fbe939ee11337e4529c80eced28b0419223111
iaconom821 <iaconom821@gmail.com>
2021-04-27 16:59:54 -0400

    commit tree 33df88dda9b929fb0caec5360891bd57aa2e747e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        self.roster[grade] ||= []
+        self.roster[grade].push(student)
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+    def sort
+        self.roster.each do |grade, students|
+            students.sort! 
+        end 
+    end
+
+
+end
